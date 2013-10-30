be5a7e29b54a82af243a2544b9d947497279f520
Pengyu CHEN <cpy.prefers.you@gmail.com>
2013-10-31 03:34:02 +0800

    fixed: more accurate touchscreen swipe and tap fixed: '.' in the key event

--- smash_android.py
+++ smash_android.py
@@ -49,16 +49,19 @@ class smashed_UI(object):
         return
 
     oldpos = None
+    is_swipe = False
 
     def button_press(self, event):
-        subprocess.call('adb shell input touchscreen tap %d %d'
-            %(event.x, event.y), shell=True)
-        print('adb shell input touchscreen tap %d %d'%(event.x, event.y))
         self.oldpos = (event.x, event.y)
         return
 
     def button_release(self, event):
+        if not self.is_swipe:
+            subprocess.call('adb shell input touchscreen tap %d %d'
+                %(event.x, event.y), shell=True)
+            print('adb shell input touchscreen tap %d %d'%(event.x, event.y))
         self.oldpos = None
+        self.is_swipe = False
         return
 
     def button_motion(self, event):
@@ -67,6 +70,7 @@ class smashed_UI(object):
                 %(self.oldpos[0], self.oldpos[1], event.x, event.y), shell=True)
             print('adb shell input touchscreen swipe %d %d %d %d'
                 %(self.oldpos[0], self.oldpos[1], event.x, event.y))
+            self.is_swipe = True
         return
 
     def key_pressed(self, event):
@@ -88,7 +92,7 @@ class smashed_UI(object):
             keycode = ord(key) - ord('a') + 29
         elif key == ',':
             keycode = 55
-        elif key == ',':
+        elif key == '.':
             keycode = 56
         elif key == '\t':
             keycode = 61
